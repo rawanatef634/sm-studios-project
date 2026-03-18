1e6eb81c0496bf58fe333f15c4a153ed3fec5d13
rawanatef634 <atron944@gmail.com>
2026-03-18 13:15:06 +0200

    Fix Hero import path case

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,26 +5,26 @@ import Clients from "../components/Clients";
 import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
-import HeroSection from "../Components/Hero";
+import HeroSection from "../components/Hero";
 
 export default function Home() {
   return (
     <>
       {/* Hero Section */}
-     <HeroSection />
+      <HeroSection />
 
       {/* Services Section */}
       <Services />
 
       {/* About Section */}
-        <About />
+      <About />
 
       {/* Team Section */}
-        <Team />
+      <Team />
 
       {/* Clients Section */}
       <Clients />
-      
+
       {/* Projects Showcase */}
       <Projects />
 
