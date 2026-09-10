95e410ada993f7cfdcf9dd0a74058e5c7eaa38f9
rawanatef634 <atron944@gmail.com>
2026-09-10 04:39:40 +0300

    test: introduce carousel bug for HeronAgent QA

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -316,7 +316,7 @@ function DesktopStage({ projects, index }) {
 }
 
 function MobileStage({ projects, index, direction }) {
-  const center = projects[wrapIndex(index, projects.length)];
+  const center = projects[index];
   const enterX = direction >= 0 ? 24 : -24;
   const exitX = direction >= 0 ? -24 : 24;
 
